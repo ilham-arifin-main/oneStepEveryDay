5294fd750c531b70ed979e146af59d62e4a83fa3
Ilham Arifin <ilhamarifin.main@gmail.com>
2026-01-01 23:07:16 +0800

    Update app.py

--- menentukan_FPB/app.py
+++ menentukan_FPB/app.py
@@ -1,1 +1,25 @@
+def main ():
+    print("MENENTUKAN FPB")
+    print("Input bilangan:")
+    num1 = int(input())
+    num2 = int(input())
 
+    def menentukan_fpb(num1, num2):
+        x = num1 if num1 > num2 else num2
+        y = num2 if num2 < num1 else num1
+
+        while True:
+            z = x % y
+
+            if (z == 0):
+                return y
+            
+            x = y
+            y = z
+        
+    
+    hasil = menentukan_fpb(num1, num2)
+    print(f"FPB dari {num1} dan {num2} adalah {hasil}")
+
+if __name__ :
+    main()
